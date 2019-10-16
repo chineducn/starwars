a807e2f71b4885813815243ed27f472456909787
unknown <neduvinci@gmail.com>
2019-10-16 19:08:56 +0100

    added second films paragraph for visual test

--- src/components/Character.js
+++ src/components/Character.js
@@ -10,13 +10,14 @@ const CoolDiv = cutesy.div`
     border-radius: 0.5rem;
     margin: 0.3rem;
     width: 20%;
+    line-height: 0.5rem;
 `;
 
 const Character = ({ person }) => {
     return <CoolDiv>
         <p>Name: {person.name}</p>
         <p>Gender: {person.gender}</p>
-        <p>Gender: {person.gender}</p>
+        <p>Films: {person.films.length}</p>
 
     </CoolDiv>
 };
